1907813153a2e6a9b17171785fbba6755af76ebb
Aleksa Pavlovic <ap20160012@gmail.com>
2022-12-01 23:00:55 +0100

    Add ArticleEndpointPost

---
+++ Shop.Api/Articles/ArticleEndpointPost.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Api.Interfaces;
+
+namespace Shop.Api.Articles
+{
+    public class ArticleEndpointPost : IEndpoint
+    {
+        public void MapEndpoints(IEndpointRouteBuilder app)
+        {
+           
+        }
+    }
+}
