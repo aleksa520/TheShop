a4c1c207eb985de13d34667d579acf52167ee2c8
Aleksa Pavlovic <ap20160012@gmail.com>
2022-12-01 19:56:49 +0100

    Add properties for Article.

--- Shop.Domain/Model/Article.cs
+++ Shop.Domain/Model/Article.cs
@@ -2,5 +2,20 @@
 
 public class Article
 {
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public double Price { get; private set; }
+    public bool IsSold { get; private set; }
+    public DateTime SoldDate { get; private set; }
+    public int BuyerUserId { get; private set; }
 
+    public Article(int id, string name, double price, bool isSold, DateTime soldDate, int buyerUserId)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+        IsSold = isSold;
+        SoldDate = soldDate;
+        BuyerUserId = buyerUserId;
+    }
 }
