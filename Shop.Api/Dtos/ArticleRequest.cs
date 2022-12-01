cc4077d0562d8cbd224b13e2c70c3d339bf2eb3e
Aleksa Pavlovic <ap20160012@gmail.com>
2022-12-01 20:55:24 +0100

    Add Article dtos

---
+++ Shop.Api/Dtos/ArticleRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Shop.Api.Dtos;
+
+public record ArticleRequest(int Id, string Name, double Price, bool IsSold, DateTime SoldDate, int BuyerUserId);
---
+++ Shop.Api/Dtos/ArticleResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace Shop.Api.Dtos;
+
+public record ArticleResponse(int Id, string Name, double Price, bool IsSold, DateTime SoldDate, int BuyerUserId);
