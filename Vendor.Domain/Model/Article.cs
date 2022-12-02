62cb117d6104c31bb6d34a717146874efd559362
Aleksa Pavlovic <ap20160012@gmail.com>
2022-12-02 12:48:21 +0100

    Add Article model in Vendor.

---
+++ Vendor.Domain/Model/Article.cs
@@ -0,0 +1,20 @@
+﻿namespace Vendor.Domain.Model;
+
+public class Article
+{
+    public int Id { get; set; }
+    public string Name { get; private set; }
+    public double Price { get; private set; }
+
+    public Article(int id, string name, double price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+    public Article()
+    {
+        // empty construcor for automapper
+    }
+}
