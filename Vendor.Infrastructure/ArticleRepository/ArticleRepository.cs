1d8c5ba073a64df774c9289860ad73f772c19dc2
Aleksa Pavlovic <ap20160012@gmail.com>
2022-12-02 12:55:27 +0100

    Add ArticleRepository for Vendor.

---
+++ Vendor.Infrastructure/ArticleRepository/ArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Vendor.Domain.Model;
+
+namespace Vendor.Infrastructure.ArticleRepository;
+
+public class ArticleRepository : IArticleRepository
+{
+    public async Task<bool> ArticleInInventory(int id)
+    {
+        return new Random().NextDouble() >= 0.5;
+    }
+
+    public async Task<Article> GetArticle(int id)
+    {
+        return new Article(id, $"Article {id}", new Random().Next(100, 500));
+    }
+}
---
+++ Vendor.Infrastructure/ArticleRepository/IArticleRepository.cs
@@ -0,0 +1,9 @@
+﻿using Vendor.Domain.Model;
+
+namespace Vendor.Infrastructure.ArticleRepository;
+
+public interface IArticleRepository
+{
+    public Task<bool> ArticleInInventory(int id);
+    public Task<Article> GetArticle(int id);
+}
