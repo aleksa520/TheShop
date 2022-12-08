b5ed683735b5a1f0fb1af649ae2bd13a82fd7c34
Aleksa Pavlovic <ap20160012@gmail.com>
2022-12-08 15:05:23 +0100

    Add validator for article with FluentValidation.

---
+++ Shop.Application/Article/Command/BuyArticle/BuyArticleCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Shop.Application.Article.Command.BuyArticle;
+
+public class BuyArticleCommandValidator : AbstractValidator<BuyArticleCommand>
+{
+	public BuyArticleCommandValidator()
+	{
+		RuleFor(a => a).NotNull();
+		RuleFor(a => a.Name).NotEmpty();
+		RuleFor(a => a.Price).NotEmpty();
+		RuleFor(a => a.BuyerUserId).NotEmpty();
+	}
+}
